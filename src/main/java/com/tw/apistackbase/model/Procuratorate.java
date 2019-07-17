0e42501f3af84785eefc38b790d89c2f1e412469
Akin <akin.gan@oocl.com>
2019-07-17 22:13:25 +0800

    feat: [Story 3.1 AC 1]

---
+++ src/main/java/com/tw/apistackbase/model/Procuratorate.java
@@ -0,0 +1,34 @@
+package com.tw.apistackbase.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Procuratorate {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
