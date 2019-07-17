b77309549aa0f139fa1cbe91ddddae9c3c4afcc9
Akin <akin.gan@oocl.com>
2019-07-17 22:35:35 +0800

    feat: [story 3.2 AC2]

---
+++ src/main/java/com/tw/apistackbase/repository/ProsecutorRepository.java
@@ -0,0 +1,7 @@
+package com.tw.apistackbase.repository;
+
+import com.tw.apistackbase.model.Prosecutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProsecutorRepository extends JpaRepository<Prosecutor, Long> {
+}
