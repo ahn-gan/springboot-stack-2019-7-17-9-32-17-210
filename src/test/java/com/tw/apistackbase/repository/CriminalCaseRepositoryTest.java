dc45277921ca769006013ac5bcd897217b2dd08f
Akin <akin.gan@oocl.com>
2019-07-17 21:09:00 +0800

    feat: [Story 1 AC5] & modify the setUp.

--- src/test/java/com/tw/apistackbase/repository/CriminalCaseRepositoryTest.java
+++ src/test/java/com/tw/apistackbase/repository/CriminalCaseRepositoryTest.java
@@ -28,24 +28,32 @@ public class CriminalCaseRepositoryTest {
         criminalCases = new ArrayList<>();
         criminalCases.add(new CriminalCase("test-case-1", 1600));
         criminalCases.add(new CriminalCase("test-case-2", 1700));
+        criminalCases.add(new CriminalCase("test-case-3", 1800));
         criminalCaseRepository.saveAll(criminalCases);
     }
 
     @Test
     public void should_return_all_criminal_cases_when_find_all() {
         List<CriminalCase> findCriminalCases = criminalCaseRepository.findAll();
-        Assertions.assertEquals(findCriminalCases.size(), 2);
+        Assertions.assertEquals(findCriminalCases.size(), 3);
     }
 
     @Test
     public void should_return_all_criminal_cases_order_by_time_desc_when_find_all_with_order() {
         List<CriminalCase> findCriminalCases = criminalCaseRepository.findAllByOrderByOccurrenceTimeDesc();
-        Assertions.assertEquals(findCriminalCases.get(0).getOccurrenceTime(), 1700);
+        Assertions.assertEquals(findCriminalCases.get(0).getOccurrenceTime(), 1800);
     }
 
     @Test
     public void should_criminal_cases_when_find_by_name() {
         List<CriminalCase> findCriminalCases = criminalCaseRepository.findAllByCaseName("test-case-1");
         Assertions.assertEquals(findCriminalCases.get(0).getCaseName(), "test-case-1");
+    }
+
+    @Test
+    public void should_delete_count_when_delete_by_id() {
+        criminalCaseRepository.deleteById(Long.valueOf(1));
+        List<CriminalCase> findCriminalCases = criminalCaseRepository.findAll();
+        Assertions.assertEquals(findCriminalCases.size(), 2);
     }
 }
